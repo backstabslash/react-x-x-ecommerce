89d4d588d3b5e6b7fb6c5fdc7b55062269ddebf9
backstabslash <mr.toppanamor1@gmail.com>
2023-04-13 09:23:16 +0300

    header + utils

---
+++ src/components/header/header.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import "./header.css";
+
+function Header() {
+  const [headerFixed, setheaderFixed] = useState(false);
+
+  useEffect(() => {
+    const onPageScroll = () => {
+      if (window.pageYOffset > 0) {
+        setheaderFixed(true);
+      } else {
+        setheaderFixed(false);
+      }
+    };
+    window.addEventListener("scroll", onPageScroll);
+  }, []);
+
+  return (
+    <header
+      className={`header container ${headerFixed ? "header__fixed" : ""}`}
+    >
+      <div className="header__wrapper">
+        <div className="header__brand">SHOESHOP</div>
+        <nav>
+          <ul className="header__menu">
+            <li>
+              <a className="header__link" href="">
+                Categories
+              </a>
+            </li>
+            <li>
+              <a className="header__link" href="">
+                Product
+              </a>
+            </li>
+            <li className="header__line"></li>
+            <li>
+              <button className="header__sun">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                </svg>
+              </button>
+            </li>
+            <li>
+              <button className="header__cart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                </svg>
+              </button>
+            </li>
+          </ul>
+          <button className="header__bars">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
