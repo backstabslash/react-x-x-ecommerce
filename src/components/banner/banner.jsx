85f6710972d625e2d537902482d8d2b221562e4a
backstabslash <mr.toppanamor1@gmail.com>
2023-04-15 23:53:52 +0300

    dxdx

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,13 +1,12 @@
 import "./banner.css";
+import Img1 from "../../img/banners/banner.jpg";
 
 function Banner() {
   return (
     <section className="banner container section">
       <div className="banner__wrapper">
         <div className="banner__description">
-          <h2 className="banner__description-title">
-            Step into something new today
-          </h2>
+          <h2 className="banner__description-title">Step into something new</h2>
           <p className="banner__description-text">
             Step up your shoe game with our extensive collection of trendy
             footwear - shop now at Shoeshop and step out in style!
@@ -15,7 +14,7 @@ function Banner() {
           <button className="banner__description-btn btn">SHOP NOW</button>
         </div>
         <div className="banner__img">
-          <img src="./banner.jpg" alt="" />
+          <img src={Img1} alt="banner" />
         </div>
       </div>
     </section>
--- src/components/proud/proudItem.jsx
+++ src/components/proud/proudItem.jsx
@@ -5,7 +5,7 @@ function ProudItem({ item }) {
     <div className="proud__item">
       <a href="">
         <div className="proud__item-header">
-          <img src={item.img} alt="1"></img>
+          <img src={item.img} alt={item.id + " " + item.description}></img>
         </div>
         <div className="proud__item-description">
           <p className="proud__item-label">{item.description}</p>
