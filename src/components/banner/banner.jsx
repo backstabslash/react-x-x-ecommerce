3268344627c5833562fcf1b546d1079df3a0e63a
backstabslash <mr.toppanamor1@gmail.com>
2023-04-15 22:36:06 +0300

    proud section

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,12 @@
+import "./banner.css";
+
+function Banner() {
+  return (
+    <section className="banner container section">
+      <h2 className="proud__title">Pedals we're proud of: </h2>
+      <div className="proud__item-container"></div>
+    </section>
+  );
+}
+
+export default Banner;
